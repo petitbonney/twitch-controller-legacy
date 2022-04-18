a23be66fd10593c0fc21e29f13223dbeb0fc076b
petitbonney <petitbonney@gmail.com>
2022-04-18 12:22:41 +0200

    Fix commands index.js .at error

--- commands/index.js
+++ commands/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
-const files = fs.readdirSync(__filename.split('\\').at(-2))
-files.splice(files.indexOf(__filename.split('\\').at(-1)), 1)
+const path = require('path');
+const split = __filename.split(path.sep)
+const files = fs.readdirSync(split[split.length-2])
+files.splice(files.indexOf(split[split.length-1]), 1)
 files.forEach(f => {
     exports[f.split('.')[0]] = require('./' + f)
 });
